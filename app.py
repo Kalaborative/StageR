5cb8d6f77122ad7377d5e388f43dfddae471f605
Kalaborative <kalaborative94@gmail.com>
2017-06-28 13:45:15 -0500

    documentation

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
+# import the necessary modules
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 from os import environ
 
+# start a Flask app
 app = Flask(__name__)
 
+# Declare global variables
 firstUserData = []
 
+# The makeata function handles new user registration on any page.
 def makedata():
 	with sqlite3.connect("tags.db") as connection:
 		c = connection.cursor()
+		# pull name data from history table
 		c.execute("SELECT name FROM history")
 		histdata = c.fetchall()
+		# this code flattens the list
 		histdata = [h[0] for h in histdata]
 		inputsex = request.form["sex"]
 		inputname = request.form["discordtag"]
+		# declare a match var that will tell if the for loop finds a match or not
 		match = False
+		# the default batch
 		batch = [(inputsex, inputname, 0)]
 		for hist in histdata:
 			if inputname == hist:
@@ -26,6 +34,7 @@ def makedata():
 		if not match:
 			c.executemany("INSERT INTO history VALUES(?,?,?)", batch)
 
+# the default route decorator
 @app.route("/", methods=["GET", "POST"])
 def index():
 	if request.method == "POST":
@@ -36,12 +45,14 @@ def index():
 		c.execute("SELECT * FROM current")
 		myUsers = c.fetchall()
 		global firstUserData
+		# if the lenggth of users is more than 0, we need to pull the first name
 		if len(myUsers) > 0:
 			firstUserData = myUsers.pop(-0)
 		else:
 			firstUserData = None
 		return render_template("index.html", users=myUsers, first=firstUserData)
 
+# decorator for the 'next' button
 @app.route('/next', methods=["GET", "POST"])
 def nextSinger():
 	if request.method == "POST":
@@ -58,6 +69,8 @@ def nextSinger():
 			return render_template("index.html", users=myUsers, first=firstUserData)
 	with sqlite3.connect("tags.db") as connection:
 		c = connection.cursor()
+		# when next is clicked, we need to increment the times he/she has sang by 1.
+		# as well as remove the user from the 'current' queue.
 		singer = (firstUserData[1])
 		c.execute("UPDATE history SET singTimes = singTimes + 1 WHERE name=?", [singer])
 		c.execute("DELETE FROM current WHERE name=?", [singer])
@@ -69,7 +82,7 @@ def nextSinger():
 			firstUserData = None
 		return render_template("index.html", users=myUsers, first=firstUserData)
 
-
+# decorator for the 'skip' button
 @app.route('/skip', methods=["GET", "POST"])
 def skipSinger():
 	if request.method == "POST":
@@ -87,6 +100,7 @@ def skipSinger():
 	with sqlite3.connect("tags.db") as connection:
 		c = connection.cursor()
 		singer = (firstUserData[1])
+		# here, we don't need to increment. Just delete the record in current.
 		c.execute("DELETE FROM current WHERE name=?", [singer])
 		c.execute("SELECT * FROM current")
 		myUsers = c.fetchall()
@@ -97,30 +111,8 @@ def skipSinger():
 		return render_template("index.html", users=myUsers, first=firstUserData)
 
 
-@app.route('/start', methods=["GET", "POST"])
-def startQueue():
-	if request.method == "POST":
-		makedata()
-		with sqlite3.connect("tags.db") as connection:
-			c = connection.cursor()
-			c.execute("SELECT * FROM current")
-			myUsers = c.fetchall()
-			global firstUserData
-			if len(myUsers) > 0:
-				firstUserData = myUsers.pop(-0)
-			else:
-				firstUserData = None
-			return render_template("index.html", users=myUsers, first=firstUserData)
-	with sqlite3.connect("tags.db") as connection:
-		c = connection.cursor()
-		c.execute("SELECT * FROM current")
-		myUsers = c.fetchall()
-		if len(myUsers) > 0:
-			firstUserData = myUsers.pop(-0)
-		else:
-			firstUserData = None
-		return render_template("index.html", users=myUsers, first=firstUserData)
-
 if __name__ == "__main__":
+	# convention to run on Heroku
 	port = int(environ.get("PORT", 5000))
+	# run the app available anywhere on the network, on debug mode
 	app.run(host='0.0.0.0', port=port, debug=True)
